b579c12eade4621a1c4cbf54eb23984a62c10cd4
imak02 <69449184+imak02@users.noreply.github.com>
2020-08-14 13:42:41 +0545

    Big one

---
+++ BigProject/javascript.js
@@ -0,0 +1,97 @@
+
+function functionbg1()
+{
+	var obj1=document.getElementById("bg1");
+	var obj2=document.getElementById("bg2");
+	var obj3=document.getElementById("bg3");
+	var obj4=document.getElementById("bh1");
+	var obj5=document.getElementById("bh2");
+	var obj6=document.getElementById("bh3");
+	obj1.style.visibility="hidden";
+	obj2.style.visibility="hidden";
+	obj3.style.visibility="visible";
+	obj4.style.visibility="hidden";
+	obj5.style.visibility="hidden";
+	obj6.style.visibility="visible";
+	
+}		
+function functionbg2()
+{
+	var obj1=document.getElementById("bg1");
+	var obj2=document.getElementById("bg2");
+	var obj3=document.getElementById("bg3");
+	var obj4=document.getElementById("bh1");
+	var obj5=document.getElementById("bh2");
+	var obj6=document.getElementById("bh3");
+	obj1.style.visibility="hidden";
+	obj2.style.visibility="visible";
+	obj3.style.visibility="hidden";
+	obj4.style.visibility="hidden";
+	obj5.style.visibility="visible";
+	obj6.style.visibility="hidden";
+	
+}	
+function functionbg3()
+{
+	var obj1=document.getElementById("bg1");
+	var obj2=document.getElementById("bg2");
+	var obj3=document.getElementById("bg3");
+	var obj4=document.getElementById("bh1");
+	var obj5=document.getElementById("bh2");
+	var obj6=document.getElementById("bh3");
+	obj1.style.visibility="visible";
+	obj2.style.visibility="hidden";
+	obj3.style.visibility="hidden";
+	obj4.style.visibility="visible";
+	obj5.style.visibility="hidden";
+	obj6.style.visibility="hidden";
+	
+}
+function functionbg4()
+{
+	var obj1=document.getElementById("bg1");
+	var obj2=document.getElementById("bg2");
+	var obj3=document.getElementById("bg3");
+	var obj4=document.getElementById("bh1");
+	var obj5=document.getElementById("bh2");
+	var obj6=document.getElementById("bh3");
+	obj1.style.visibility="hidden";
+	obj2.style.visibility="hidden";
+	obj3.style.visibility="visible";
+	obj4.style.visibility="hidden";
+	obj5.style.visibility="hidden";
+	obj6.style.visibility="visible";
+	
+}
+function functionbg5()
+{
+	var obj1=document.getElementById("bg1");
+	var obj2=document.getElementById("bg2");
+	var obj3=document.getElementById("bg3");
+	var obj4=document.getElementById("bh1");
+	var obj5=document.getElementById("bh2");
+	var obj6=document.getElementById("bh3");
+	obj1.style.visibility="hidden";
+	obj2.style.visibility="visible";
+	obj3.style.visibility="hidden";
+	obj4.style.visibility="hidden";
+	obj5.style.visibility="visible";
+	obj6.style.visibility="hidden";
+	
+}
+function functionbg6()
+{
+	var obj1=document.getElementById("bg1");
+	var obj2=document.getElementById("bg2");
+	var obj3=document.getElementById("bg3");
+	var obj4=document.getElementById("bh1");
+	var obj5=document.getElementById("bh2");
+	var obj6=document.getElementById("bh3");
+	obj1.style.visibility="visible";
+	obj2.style.visibility="hidden";
+	obj3.style.visibility="hidden";
+	obj4.style.visibility="visible";
+	obj5.style.visibility="hidden";
+	obj6.style.visibility="hidden";
+	
+}
